83252d922bef4399653e7b80ff70dad0743bcc6d
wk645 <wk645@nyu.edu>
2017-08-03 13:35:47 -0400

    almost done

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -11,9 +11,6 @@ class Picture < ApplicationRecord
   def self.trending
     trending = Picture.all.sort_by {|picture| picture.created_at}.first(10)
       trending.sort_by { |picture| picture.comments.count}.reverse.first(3)
-    #of the 10 most recent pictures
-    #find the picture with the most comments
-    #return the tag(s) associated with that picture
   end
 
 end
